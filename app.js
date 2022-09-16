5073648786b9da662fde5589f8f0672b36349926
adeolammy <adeolammy2020@gmail.com>
2022-09-16 13:13:07 +0100

    more modifications

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-$(function () {
-  $('.popover-dismiss').popover({
-    container: 'body'
-  })
+
+
+// SIGN IN / LOG-IN PART  / SIGN IN / LOG-IN PART  / SIGN IN / LOG-IN PART  
+// SIGN IN / LOG-IN PART  / SIGN IN / LOG-IN PART  / SIGN IN / LOG-IN PART  
+  
+
+ const signupBtn = document.querySelector(".signupBtn");
+const loginBtn = document.querySelector(".loginBtn");
+const moveBtn = document.querySelector(".moveBtn");
+const signup = document.querySelector(".signup");
+const login = document.querySelector(".login");
+
+loginBtn.addEventListener('click', ()=>{
+  moveBtn.classList.add('rightBnt');
+  login.classList.add('loginForm');
+  signup.classList.remove('signUpForm');
+  moveBtn.innerHTML = 'Log-In'
 })
+
+signupBtn.addEventListener('click', ()=>{
+  moveBtn.classList.remove('rightBnt');
+   login.classList.remove('loginForm');
+  signup.classList.add('signUpForm');
+  moveBtn.innerHTML = 'Sign-Up'
+})
+
+
+
+
+
+// /////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////
+
+  
